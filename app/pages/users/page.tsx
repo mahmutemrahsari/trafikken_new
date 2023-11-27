51c4bd96b6bf142543ba47af0ece2c4ab7dae55e
Mahmut Emrah Sari <mahmutemrahsari@gmail.com>
2023-11-27 10:48:55 +0100

    some page.tsx files added

--- app/pages/users/page.tsx
+++ app/pages/users/page.tsx
@@ -6,12 +6,13 @@ interface User {
 }
 
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users"); //cache is active!
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <h3>{new Date().toLocaleTimeString()}</h3>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
